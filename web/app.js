5ebf4d166a3fa0fd19d807eb54592fd59cbd41bb
Norbert Schmidt <norbert.schmidt@mayflower.de>
2012-10-12 13:35:09 +0200

    Removed obsolete members from application object

--- web/app.js
+++ web/app.js
@@ -23,12 +23,6 @@ Ext.application({
 
     autoCreateViewport: true,
 
-    messageBus: null,
-
-    defaultModule: {
-        module: 'employee'
-    },
-
     init: function() {
         var me = this;
 
